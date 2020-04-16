5979f7f1a72673e11f7ab73a873a92de05904de3
zhaoming-026 <zhaoming-026@cpic.com.cn>
2020-04-16 16:39:41 +0800

    1.新增mysql事物和隔离级别知识点

---
+++ java/thread-related/src/com/tkming/thread/pool/PoolCache.java
@@ -0,0 +1,51 @@
+package com.tkming.thread.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author zhaoming-026
+ * @version 1.0
+ * @date 2020/3/31
+ * @description 缓存线程池，核心线程数0，最大线程数Integer.MAX_VALUE，不处理任务的非核心线程存活时间为60s，
+ * SynchronousQueue，非核心线程启动过多容易导致oom
+ */
+public class PoolCache {
+    public static void main(String[] args) {
+        ExecutorService single = Executors.newCachedThreadPool();
+        single.execute(new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("single");
+        }));
+        single.shutdown();
+        //shutdown不再往队列添加任务，shutdownNow会试图终止正在执行的任务
+        /*single.shutdownNow();*/
+        System.out.println("main continue.");
+        ExecutorService single2 = Executors.newSingleThreadExecutor(Executors.defaultThreadFactory());
+        single2.execute(new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("single2");
+        }));
+        single2.shutdown();
+        System.out.println("main continue..");
+        ExecutorService single3 = Executors.newSingleThreadExecutor(Executors.defaultThreadFactory());
+        single3.execute(new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("single3");
+        }));
+        single3.shutdown();
+        System.out.println("main continue...");
+    }
+}
---
+++ java/thread-related/src/com/tkming/thread/pool/PoolFixed.java
@@ -0,0 +1,51 @@
+package com.tkming.thread.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author zhaoming-026
+ * @version 1.0
+ * @date 2020/3/31
+ * @description 固定线程池，核心线程数n，最大线程数n，初始化时指定，不处理任务的非核心线程存活时间为0，
+ * 无界队列LinkedBlockingQueue的初始大小为Integer.MAX_VALUE，队列元素过多容易导致oom
+ */
+public class PoolFixed {
+    public static void main(String[] args) {
+        ExecutorService single = Executors.newFixedThreadPool(1);
+        single.execute(new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("single");
+        }));
+        single.shutdown();
+        //shutdown不再往队列添加任务，shutdownNow会试图终止正在执行的任务
+        /*single.shutdownNow();*/
+        System.out.println("main continue.");
+        ExecutorService single2 = Executors.newSingleThreadExecutor(Executors.defaultThreadFactory());
+        single2.execute(new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("single2");
+        }));
+        single2.shutdown();
+        System.out.println("main continue..");
+        ExecutorService single3 = Executors.newSingleThreadExecutor(Executors.defaultThreadFactory());
+        single3.execute(new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("single3");
+        }));
+        single3.shutdown();
+        System.out.println("main continue...");
+    }
+}
